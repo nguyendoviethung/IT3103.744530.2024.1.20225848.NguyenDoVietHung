861ebe5afd92cf6c04e3baa80957f5249f213667
viethung21072004 <167107987+viethung21072004@users.noreply.github.com>
2024-09-29 15:49:14 +0700

    Delete FirstDialog.java

--- FirstDialog.java
+++
@@ -1,9 +0,0 @@
-// Example 2: FirstDialog.java
-import javax.swing.JOptionPane;
-
-public class FirstDialog {
-    public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null, "I love you so much <3");
-        System.exit(0);
-    }
-}
